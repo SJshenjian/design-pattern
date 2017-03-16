195d04856a3a43176d2dfb0cd60e2af2516c2771
SJsunshine <shenjian66666688@sina.com>
2017-03-16 22:01:35 +0800

    懒汉

--- src/singleton/User.java
+++ src/singleton/User.java
@@ -9,17 +9,41 @@ public class User {
 	/*
 	 * 懒汉式单例类，在第一次调用时进行实例化
 	 */
+	//懒汉线程安全实现
+	/*
+	 * //1.在newInstance方法上加同步
 	private static User user=null;
-	public static User newInstance(){
+	public static synchronized User newInstance(){
 		if(user==null){
 			user=new User();
 		}
 		return user;
+	}*/
+	/*
+	//2  双重检查锁定   不是线程安全的，如果要用这种方式，需要使用volatile关键字
+	private static volatile User user=null;
+	public static User newInstance(){
+		if(user==null){
+			synchronized(User.class){
+				if(user==null){
+					return new User();
+				}
+			}
+		}
+		return user;
+	}*/
+	
+	//3静态内部类
+	private static class LazyHolder{
+		private static final User USER=new User();
 	}
 	
+	public static User newInstance(){
+		return LazyHolder.USER;
+	}
 	
 	/*
-	 * 饿汉式单例类，在初始化时进行实例化
+	 * 饿汉式单例类，在初始化时进行实例化,天生线程安全
 	 
 	private static final User user=new User();
 	public static User getUser(){
