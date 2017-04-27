9b8c7e296666dd863f66d05ec8f3033b07ce35b4
SJsunshine <shenjian66666688@sina.com>
2017-04-27 17:17:44 +0800

    说明

--- src/factoryPattern/simplefactory/SimpleShapeFactory.java
+++ src/factoryPattern/simplefactory/SimpleShapeFactory.java
@@ -7,6 +7,16 @@ import factoryPattern.simplefactory.service.impl.Square;
 
 public class SimpleShapeFactory {
 
+	/**
+	 * 核心:具体的实现由其Shape子类实现,返回其本身Shape对象
+	 * 优点:工厂方法getShape为静态方法，可直接调用；
+	 *      根据参数获取相应对象，无须知道内部实现；
+	 * 缺点:
+	 *    增加产品，需改变工厂类逻辑条件，与开闭原则相违背；
+	 *    一旦该工厂方法出现故障，其他产品受到影响
+	 * @param shapeType
+	 * @return
+	 */
 	public static Shape getShape(String shapeType){
 		if(null == shapeType){
 			return null;
