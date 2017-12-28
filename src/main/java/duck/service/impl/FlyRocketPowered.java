b06883f9efb5e1aa23129d8e4e2738b5a3c103e5
SJshenjian <shenjian66666688@sina.com>
2017-12-28 15:04:36 +0800

    设计模式初探之Duck示例

--- src/main/java/duck/service/impl/FlyRocketPowered.java
+++ src/main/java/duck/service/impl/FlyRocketPowered.java
@@ -3,7 +3,7 @@ package duck.service.impl;
 import duck.service.FlyBehavior;
 
 public class FlyRocketPowered implements FlyBehavior {
-
+    
     @Override
     public String fly() {
         return "I'm flying with a rocket";
