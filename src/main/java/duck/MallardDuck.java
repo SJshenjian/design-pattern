ff83001be211c21d8ab46fab24a5e724ebf96670
SJshenjian <shenjian66666688@sina.com>
2017-12-28 15:02:43 +0800

    设计模式初探之Duck示例

--- src/main/java/duck/MallardDuck.java
+++ src/main/java/duck/MallardDuck.java
@@ -5,7 +5,6 @@ import duck.service.impl.FlyWithWings;
 import duck.service.impl.Quack;
 
 public class MallardDuck extends Duck{
-
 	public MallardDuck(){
 		flyBehavior=new FlyWithWings();
 		quackBehavior=new Quack();
