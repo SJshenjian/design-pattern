f78421bf4380ec80ff116f482406742e6c7c824c
shenjian <sjshenjian@outlook.com>
2019-02-24 16:43:21 +0800

    闭锁优化

--- src/main/java/concurrent/module/TestHarness.java
+++ src/main/java/concurrent/module/TestHarness.java
@@ -3,38 +3,47 @@ package concurrent.module;
 import java.util.concurrent.CountDownLatch;
 
 /**
+ * 闭锁实现统计多个线程执行时间
+ *
  * @author : Jian Shen
  * @version : V1.0
  * @date : 2018/5/26
  */
 public class TestHarness {
 
-    public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
-        final CountDownLatch startGate = new CountDownLatch(1);
-        final CountDownLatch endGate = new CountDownLatch(nThreads);
+   public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
+       CountDownLatch startGate = new CountDownLatch(1);
+       CountDownLatch endGate = new CountDownLatch(nThreads);
 
-        for (int i = 0; i < nThreads; i++) {
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        startGate.await();
-                        try {
-                            task.run();
-                        } finally {
-                            endGate.countDown();
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
+       for (int i = 0 ; i < nThreads; i++) {
+           new Thread(() -> {
+               try {
+                   startGate.await();
+                   try {
+                       task.run();
+                   } finally {
+                       endGate.countDown();
+                   }
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+           }).start();
+       }
 
-        long start = System.nanoTime();
-        startGate.countDown();
-        endGate.await();
-        long end =System.nanoTime();
-        return end - start;
+       long startTime = System.nanoTime();
+       startGate.countDown();
+       endGate.await();
+       long endTime = System.nanoTime();
+       return endTime - startTime;
+   }
+
+    public static void main(String[] args) throws InterruptedException {
+       Runnable runnable = () -> {
+           for (int i = 0; i < 10000; i++) {
+
+           }
+       };
+        long time = new TestHarness().timeTasks(4, runnable);
+        System.out.println(time);
     }
 }
